8cf65f0fd8c0d66120353c980d1a7c0a3e9e48c4
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-08-08 17:56:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(listFriends, celebratedDay){
+    let gratitudeCard = [];
+    for (let i=0; i<listFriends.length;i++){
+      gratitudeCard.push(`Thank you, ${listFriends[i]}, for the wonderful ${celebratedDay} gift!`);
+    }
+
+    return gratitudeCard;
+}
+
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+function countDown(i){
+    while (i > 0){
+        console.log(i--);
+    }
+    console.log(i);
+}
+
+countDown(10);
